764400a7399a5e4a7e5fc732179916b8134be902
unknown <Tirtayudha28>
2025-06-14 13:07:35 +0700

    Update Lagi

--- script.js
+++ script.js
@@ -224,6 +224,14 @@ allMenuLinks.forEach(link => {
 });
 
 
+  window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    navbar.classList.toggle('scrolled', window.scrollY > 10);
+  });
+
+  
+
+
 
 
 
